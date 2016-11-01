6b2e3fa10cc7e64181f4bd7d78b4dcaa15816f0b
Harrison Stall <hstall2529@gmail.com>
2016-11-01 11:39:22 -0500

    Added routes for GET users and equipment

--- server.py
+++ server.py
@@ -9,6 +9,14 @@ def hello():
 def hit_api():
     return "You've hit the makerspace API!"
 
+@app.route("/api/users/")
+def get_users():
+    return "Get users request made."
+
+@app.route("/api/equipment/")
+def get_equipment():
+    return "Get equipment request made"
+
 if __name__ == "__main__":
     app.run()
 
