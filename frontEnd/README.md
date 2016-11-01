5b80cd4a80a9f2fa1013ce7c15cb55b169855d4f
Joseph Stahl <josephst18+git@outlook.com>
2016-11-01 11:43:28 -0500

    hello world with React

---
+++ frontEnd/README.md
@@ -0,0 +1,6 @@
+# Installation
+Install webpack globally with `npm install -g webpack`. Then run `webpack` to build the app.
+
+## Development
+Run `npm install -g webpack-dev-server` to install the development server.
+Then start it up with `webpack-dev-server --progress --colors`.
---
+++ frontEnd/webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: './dist/bundle.js',
+  },
+
+  // sourcemaps
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.js']
+  },
+
+  module: {
+    loaders: [
+      // CSS files
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+
+      // ES 2015 features and React
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ],
+
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: 'source-map-loader' }
+    ]
+  },
+  
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM'
+  },
+};
