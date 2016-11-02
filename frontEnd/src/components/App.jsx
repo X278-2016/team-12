f7231011b7c45cb4c7a25c526fb56ce3a8a8a9a5
Joseph Stahl <josephst18+git@outlook.com>
2016-11-02 12:37:45 -0500

    fix all eslint complaints

--- frontEnd/src/components/App.jsx
+++ frontEnd/src/components/App.jsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import { IndexLink } from 'react-router';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Simple SPA</h1>
-        <ul className="header">
-          <li><IndexLink to="/">Home</IndexLink></li>
-        </ul>
-        <div className="content">
-          { this.props.children }
-        </div>
+function App(props) {
+  return (
+    <div>
+      <h1>Simple SPA</h1>
+      <ul className="header">
+        <li><IndexLink to="/">Home</IndexLink></li>
+      </ul>
+      <div className="content">
+        { props.children }
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+App.propTypes = {
+  children: React.PropTypes.string.isRequired,
+};
+
+export default App;
