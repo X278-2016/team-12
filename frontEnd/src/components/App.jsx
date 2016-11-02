5842c1424ab1baa901b4c4441218c057a04570d3
Joseph Stahl <josephst18+git@outlook.com>
2016-11-02 11:51:31 -0500

    fix most eslint issues

--- frontEnd/src/components/App.jsx
+++ frontEnd/src/components/App.jsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import { Link, IndexLink } from 'react-router';
+import { IndexLink } from 'react-router';
 
-export class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Simple SPA</h1>
-        <ul className='header'>
-          <li><IndexLink to='/'>Home</IndexLink></li>
+        <ul className="header">
+          <li><IndexLink to="/">Home</IndexLink></li>
         </ul>
-        <div className='content'>
+        <div className="content">
           { this.props.children }
         </div>
       </div>
-    )
+    );
   }
 }
--- frontEnd/src/index.jsx
+++ frontEnd/src/index.jsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import { App } from './components/App';
-import { UserInput } from './components/UserInput';
+import App from './components/App';
+import UserInput from './components/UserInput';
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path='/' component={App}>
+    <Route path="/" component={App}>
       <IndexRoute component={UserInput} />
     </Route>
   </Router>,
