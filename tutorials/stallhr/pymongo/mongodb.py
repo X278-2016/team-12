0562e78b2d32ac4510ddcaf296e0d305ce87d6c0
Harrison Stall <hstall2529@gmail.com>
2016-11-08 12:21:55 -0600

    Added tutorial on using Mongo with Flask

---
+++ tutorials/stallhr/pymongo/mongodb.py
@@ -0,0 +1,20 @@
+## mongo NoSQL, DocumentDB
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    con = MongoClient()
+
+    # Creates new TB called test_db if doesn't exist
+    db = con.test_database
+
+    # Creates member variable of our DB object
+    people = db.people
+    people.insert({'name':'Bob', 'food':'cheese'})
+    people.insert({'name':'John', 'location':'USA'})
+
+    # SELECT *
+    peeps = people.find()
+    print 'Insert & find'
+    for person in peeps:
+        print person
