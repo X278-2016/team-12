f5ca2729cfcfd82419ca10f3cdf1e4eb390013dd
Brittany Roth <brittany.d.roth@vanderbilt.edu>
2016-11-17 11:14:27 -0600

    added more functionality to admins

--- frontEnd/src/components/Admin/AdminPanel.jsx
+++ frontEnd/src/components/Admin/AdminPanel.jsx
@@ -9,11 +9,21 @@ export default class AdminPanel extends React.Component {
         return (
             <div>
                 <h1>Admin Panel</h1>
+                <button> Add Users </button>
+                <br/>
                 <button> Edit Users </button>
+                <br/>
+                <button> Add Admins </button>
                 <br/>
                 <button> Edit Admins </button>
                 <br/>
+                <button> Add Equipment </button>
+                <br/>
                 <button> Edit Equipment </button>
+                <br/>
+                <button> Add Resources </button>
+                <br/>
+                <button> Edit Resources </button>
             </div>
         );
     }
